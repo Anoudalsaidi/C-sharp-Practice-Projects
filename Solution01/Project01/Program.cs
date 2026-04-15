900d87508a248bb8df84acfc28af3d2bff6deecf
“Anoud <anoudalsaidi1993@gmail.com>
2026-04-15 11:27:51 +0400

    implement basic arithmetic operations (add, subtract, multiply, divide), handle division by zero and improve input validation

---
+++ Solution01/Project01/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("plz enter first number :");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("plz enter second number :");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+            Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+            Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+        }
+    }
+}
---
+++ Solution02/Project01/Program.cs
@@ -0,0 +1,126 @@
+﻿using System.ComponentModel.Design;
+
+namespace Project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+             
+            #region Task 1: Positive - Negative - Zero Numbers
+
+            Console.WriteLine("Enter a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num > 0)
+            {
+                Console.WriteLine("Positive number");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine("Negative number");
+            }
+            else
+            {
+                Console.WriteLine("Zero");
+            }
+            #endregion
+
+
+            #region Task 2: Even or Odd
+            Console.WriteLine("Enter an integer number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine("Even number");
+            }
+            else
+            {
+                Console.WriteLine("Odd number");
+            }
+
+
+            #endregion
+
+
+
+            #region Task 3: Syudent Grade System 
+            Console.WriteLine("Enter student score (0 - 100):");
+            int score = Convert.ToInt32(Console.ReadLine());
+
+            if (score >= 90 && score <= 100)
+            {
+                Console.WriteLine("Excellent");
+            }
+            else if (score >= 75 && score <= 89)
+            {
+                Console.WriteLine("Very Good");
+            }
+            else if (score >= 60 && score <= 74)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (score >= 50 && score <= 59)
+            {
+                Console.WriteLine("Pass");
+            }
+            else if (score >= 0 && score < 50)
+            {
+                Console.WriteLine("Below Fail");
+            }
+            else
+            {
+                Console.WriteLine("Invalid score");
+            }
+            #endregion
+
+
+
+
+            #region Task 4:Simple Login System
+            Console.WriteLine("Enter username:");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Enter password:");
+            int password = Convert.ToInt32(Console.ReadLine());
+
+            if (username == "admin" && password == 1234)
+            {
+                Console.WriteLine("Login successful");
+            }
+            else
+            {
+                Console.WriteLine("Invalid username or password");
+            }
+            #endregion
+
+
+            
+            #region // Task 5: Simple ATM System
+            int balance = 1000;
+
+            Console.WriteLine("Enter withdrawal amount:");
+            int amount = Convert.ToInt32(Console.ReadLine());
+
+            if (amount <= 0)
+            {
+                Console.WriteLine("Invalid Amount");
+            }
+            else if (amount > balance)
+            {
+                Console.WriteLine("Grater than Balance");
+            }
+            else
+            {
+                balance = balance - amount;
+                Console.WriteLine("Withdrawal Successful");
+                Console.WriteLine(" Balance Now : " + balance);
+            }
+            #endregion
+
+
+        }
+    }
+}
