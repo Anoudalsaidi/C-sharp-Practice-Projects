7d9b41158e2e9c935fd121aa9f3f000d72b505d4
“Anoud <anoudalsaidi1993@gmail.com>
2026-04-16 09:08:31 +0400

    Adding Practis For Loop Statment

---
+++ Solutions04/project01/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("My Age is 20");
+            }
+        }
+    }
+}
