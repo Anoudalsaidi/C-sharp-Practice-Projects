f8e6d35ca0c59eccadc42093bae2a8ed4af9bc0f
“Anoud <anoudalsaidi1993@gmail.com>
2026-04-16 08:32:59 +0400

    Day-2: Answer Sum of Even and Odd Numbers Task

--- Solution03/Project01/Program.cs
+++ Solution03/Project01/Program.cs
@@ -104,6 +104,37 @@
                     break;
             }
             #endregion
+
+            #region Task 5: Sum of Even and Odd Numbers
+            Console.WriteLine("Enter a positive integer N:");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            if (N <= 0)
+            {
+                Console.WriteLine("Please enter a positive number");
+            }
+            else
+            {
+                int evenSum = 0;
+                int oddSum = 0;
+
+                for (int i = 1; i <= N; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        evenSum += i; // even number
+                    }
+                    else
+                    {
+                        oddSum += i; // odd number
+                    }
+                }
+
+                Console.WriteLine("Sum of even numbers: " + evenSum);
+                Console.WriteLine("Sum of odd numbers: " + oddSum);
+            }
+            #endregion
+            
         }
     }
 }
