f5307f0d5546a9efede44bd803a0c1e670a61fd7
“Anoud <anoudalsaidi1993@gmail.com>
2026-04-19 11:37:23 +0400

    Practies Error Types

---
+++ Soluation05/Project01/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Errors Types:
+
+            #region Syntax Error
+            Console.WriteLine("Hello, World!");
+            #endregion
+
+            #region Runtime Error
+            int x = 10;
+            int y = 0;
+            int result = x / y; // Runtime Error
+            #endregion
+
+            #region Logical Error
+            int a = 10;
+            int b = 5;
+            int rresult = a - b; // المفروض جمع لكن كتبنا طرح
+            #endregion
+
+            #endregion
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
