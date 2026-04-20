bf40eb916fd7e616fc05e2c3837e3257d6280bd8
“Anoud <anoudalsaidi1993@gmail.com>
2026-04-20 09:09:03 +0400

    Adding practies in Loop Through Arrays

---
+++ Solution06/Project01/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Array:
+
+            int[] number = new int[4];
+
+            for(int i=0; i<number.Length; i++)
+            {
+                Console.WriteLine("Enter 4 numbers");
+                number[i] = Convert.ToInt32(Console.ReadLine());
+            
+            }
+            Console.WriteLine("___________________");
+            foreach (int i in number)
+            {
+                Console.WriteLine(i);
+            }
+            #endregion
+        }
+    }
+}
