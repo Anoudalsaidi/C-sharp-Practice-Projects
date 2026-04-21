a1ae4b3e20d469e304761ce4f462b6067d5756ea
“Anoud <anoudalsaidi1993@gmail.com>
2026-04-21 11:00:46 +0400

    takes rows and columns from the user, fills a 2D array with values, and displays them with their indices

---
+++ Solution07/Project01/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 2D Array:
+
+            Console.Write("Enter number of rows: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter number of cols: ");
+            int cols = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("_____________________\n");
+
+            int[,] num = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int x = 0; x < cols; x++)
+                {
+                    Console.Write($"plz Enter element at [{i},{x}]: ");
+                    num[i, x] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("\n2D Array values:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"[{i},{j}]={num[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+
+            #endregion
+
+
+        }
+    }
+}
