46ec86b0f62b5ad448d0b1d5a75fd89781e9bd7a
“Anoud <anoudalsaidi1993@gmail.com>
2026-04-22 09:33:43 +0400

    Working System.IO & Creat txt files

---
+++ Solution08/Project01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+namespace Project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Working With Files
+            File.Create("test.txt");
+
+
+            string[] data = { "Anoud", "Ahmed", "Ali" };
+            File.WriteAllLines("test.txt", data);
+
+
+            File.AppendAllText("test.txt", "\nhi");
+
+            string myfile = File.ReadAllText("test.txt");
+            Console.WriteLine(myfile);
+
+            string[] myfiles = File.ReadAllLines("test.txt");
+            Console.WriteLine(myfiles[1]);
+
+
+            #endregion
+        }
+    }
+}
