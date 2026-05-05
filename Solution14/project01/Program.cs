3d36fb487016119a6887a58a3c2ea61df2eb0586
“Anoud <anoudalsaidi1993@gmail.com>
2026-05-05 10:43:58 +0400

    Adding properties (Gitter & Setter) excersies

---
+++ Solution14/project01/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace project01
+{
+    internal class Program
+    {
+        #region properties (Gitter & Setter)
+        class car
+        {
+            public int model;
+            public string color;
+            private int speed;
+            public int Speed
+            {
+
+                get { return speed; }
+                set { if (value > 0)
+                        speed = value; }
+            }
+            static void Main(string[] args)
+            {
+                car myob = new car();
+                myob.color = "red";
+                myob.model = 2000;
+
+
+                Console.WriteLine("enter speed value");
+                myob.Speed = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(myob.Speed);
+            }
+        #endregion
+
+        }
+    }
+}
