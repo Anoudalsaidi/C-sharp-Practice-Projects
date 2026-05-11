3deaa6fd496c728ec8dac93f94a6081eb8039c1e
“Anoud <anoudalsaidi1993@gmail.com>
2026-05-11 11:00:57 +0400

    Single Responsibility Principle (SOLID Principle)

--- Solution16/project01/Program.cs
+++ Solution16/project01/Program.cs
@@ -67,51 +67,167 @@ namespace project01
     #endregion
 
     #region objects Relationship(Composition) 
-    class Room
-    {
-        public string Name;
+    //    class Room
+    //    {
+    //        public string Name;
 
-        public Room(string name)
+    //        public Room(string name)
+    //        {
+    //            Name = name;
+    //        }
+    //    }
+
+    //    class House
+    //    {
+    //        // Composition: House owns Rooms
+    //        private List<Room> rooms;
+
+    //        public House()
+    //        {
+    //            rooms = new List<Room>
+    //        {
+    //            new Room("Bedroom"),
+    //            new Room("Kitchen"),
+    //            new Room("saloon")
+    //        };
+    //        }
+
+    //        public void ShowRooms()
+    //        {
+    //            foreach (var room in rooms)
+    //            {
+    //                Console.WriteLine(room.Name);
+    //            }
+    //        }
+    //    }
+
+    //    class Program
+    //    {
+    //        static void Main(string[] args)
+    //        {
+    //            House house = new House();
+    //            house.ShowRooms();
+    //        }
+    //    }
+    //}
+    #endregion
+
+
+
+
+    #region Single Responsibility Principle (SOLID Principle)
+    class Report
+    {
+        public void Generate()
         {
-            Name = name;
+            Console.WriteLine("Generating report");
         }
     }
 
-    class House
+    class ReportSaver
     {
-        // Composition: House owns Rooms
-        private List<Room> rooms;
-
-        public House()
+        public void Save()
         {
-            rooms = new List<Room>
-        {
-            new Room("Bedroom"),
-            new Room("Kitchen"),
-            new Room("saloon")
-        };
-        }
-
-        public void ShowRooms()
-        {
-            foreach (var room in rooms)
-            {
-                Console.WriteLine(room.Name);
-            }
+            Console.WriteLine("Saving report");
         }
     }
-
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            House house = new House();
-            house.ShowRooms();
+            Report report = new Report();
+            report.Generate();
+
+            ReportSaver saver = new ReportSaver();
+            saver.Save();
         }
     }
 }
     #endregion
 
-    
+//    #region Open/Closed Principle (SOLID Principle)
+//    abstract class Discount
+//    {
+//        public abstract double GetDiscount();
+//    }
+
+//    class StudentDiscount : Discount
+//    {
+//        public override double GetDiscount() => 10;
+//    }
+
+//    class VIPDiscount : Discount
+//    {
+//        public override double GetDiscount() => 20;
+//    }
+//    #endregion
+
+//    #region Liskov Substitution Principle (SOLID Principle)
+//    class Bird { }
+
+//    class FlyingBird : Bird
+//    {
+//        public void Fly()
+//        {
+//            Console.WriteLine("Flying");
+//        }
+//    }
+
+//    class Penguin : Bird
+//    {
+//        public void Swim()
+//        {
+//            Console.WriteLine("Swimming");
+//        }
+//    }
+//    #endregion
 
 
+//    #region Interface Segregation Principle (SOLID Principle)
+//    interface IWorkable
+//    {
+//        void Work();
+//    }
+
+//    interface IEatable
+//    {
+//        void Eat();
+//    }
+
+//    class Human : IWorkable, IEatable
+//    {
+//        public void Work() => Console.WriteLine("Working");
+//        public void Eat() => Console.WriteLine("Eating");
+//    }
+
+//    class Robot : IWorkable
+//    {
+//        public void Work() => Console.WriteLine("Working");
+//    }
+//    #endregion
+
+
+//    #region Dependency Inversion Principle (SOLID Principle)
+//    interface IDatabase
+//    {
+//        void Connect();
+//    }
+
+//    class MySQLDatabase : IDatabase
+//    {
+//        public void Connect()
+//        {
+//            Console.WriteLine("MySQL Connected");
+//        }
+//    }
+
+//    class App
+//    {
+//        private IDatabase db;
+
+//        public App(IDatabase database)
+//        {
+//            db = database;
+//        }
+//    }
+//#endregion
