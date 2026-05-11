3c9778ce574bb21945f764294ee7ec9ac805c00a
“Anoud <anoudalsaidi1993@gmail.com>
2026-05-11 08:53:23 +0400

    objects Relationship (Assocoation)

---
+++ Solution16/project01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Channels;
+
+namespace project01
+{
+
+    #region objects Relationship (Assocoation)
+    class student
+    {
+        public string name;
+    }
+
+    class study
+    {
+        public string course;
+
+        public void teach(student std)
+        {
+            Console.WriteLine(std.name + " is studying " + course);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student s1 = new student();
+            s1.name = "Anoud";
+
+            study sub = new study();
+            sub.course = "C#";
+
+            sub.teach(s1);
+        }
+    }
+    #endregion
+
+
+    }
+    
+
